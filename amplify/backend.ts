520f45db25943efc14c045b6240c1bb3fb816ff8
Kale Nelson <143835703+KaleNelson007@users.noreply.github.com>
2026-04-20 20:34:55 -0500

    AWS deployment changes

---
+++ amplify/backend.ts
@@ -0,0 +1,19 @@
+import { defineBackend, defineHttpDataSource } from '@aws-amplify/backend';
+import { data } from './data/resource';
+
+/**
+ * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
+ */
+export const PythonBackend = defineHttpDataSource({
+  name: 'PythonBackend',
+  endpoint: 'https://nrt-video-backend-fixed.eba-bu4ukmxm.us-east-2.elasticbeanstalk.com',
+  authorizationConfig: {
+    signingRegion: 'us-east-2',
+    signingServiceName: 'elasticbeanstalk',
+  },
+});
+
+defineBackend({
+  data,
+  PythonBackend,
+});
